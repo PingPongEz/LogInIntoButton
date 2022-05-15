1dde16deea863d845d3cb4341dc753c17a12c9f1
Sergey <kopgamerk@gmail.com>
2022-05-15 16:42:34 +0300

    Initial Commit

---
+++ LogInIntoButton/LogInIntoButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInIntoButtonApp.swift
+//  LogInIntoButton
+//
+//  Created by Сергей Веретенников on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LogInIntoButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
